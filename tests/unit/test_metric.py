babafd958f086f2dafd302d0c1398ba26d2f9f1a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-08-09 17:07:46 -0700

    better importing in metric test

--- tests/unit/test_metric.py
+++ tests/unit/test_metric.py
@@ -2,19 +2,19 @@
 Contains tests for the base metric class.
 """
 
-import statsite.metrics as m
+from statsite.metrics import Metric
 
 class TestMetric(object):
     def test_fold_basic(self):
         """Tests folding over a normal metric returns the key/value
         using the flag as the timestamp."""
-        metrics = [m.Metric("k", 27, 123456)]
-        assert [("k", 27, 123456)] == m.Metric.fold(metrics, 0)
+        metrics = [Metric("k", 27, 123456)]
+        assert [("k", 27, 123456)] == Metric.fold(metrics, 0)
 
     def test_fold_uses_now(self):
         """
         Tests that folding over a normal metric with no flag will use
         the "now" time as the timestamp.
         """
-        metrics = [m.Metric("k", 27, None)]
-        assert [("k", 27, 123456)] == m.Metric.fold(metrics, 123456)
+        metrics = [Metric("k", 27, None)]
+        assert [("k", 27, 123456)] == Metric.fold(metrics, 123456)
