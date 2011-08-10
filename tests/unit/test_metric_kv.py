fd1d72f63dbbca01fbe4bfe7beb4f07e77bb99b4
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-08-09 18:11:40 -0700

    test KeyValue metric

---
+++ tests/unit/test_metric_kv.py
@@ -0,0 +1,20 @@
+"""
+Contains tests for the key/value metric class.
+"""
+
+from statsite.metrics import KeyValue
+
+class TestKeyValue(object):
+    def test_fold_basic(self):
+        """Tests folding over a normal metric returns the key/value
+        using the flag as the timestamp."""
+        metrics = [KeyValue("k", 27, 123456)]
+        assert [("k", 27, 123456)] == KeyValue.fold(metrics, 0)
+
+    def test_defaults_flag_to_now(self, monkeypatch):
+        """Tests that the flag is defaulted to the time of instantiation
+        if no flag is given."""
+        now = 27
+        monkeypatch.setattr(time, 'time', lambda: return now)
+        metrics = [KeyValue("k", 27)]
+        assert [("k", 27, now)] == KeyValue.fold(metrics, 0)
