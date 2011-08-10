da04ec72aca99582a88186b04919e02d5ef462a7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2011-08-10 11:45:12 -0700

    collector + tests

---
+++ statsite/collector.py
@@ -0,0 +1,113 @@
+"""
+Contains the base class for collectors as well as the built-in UDP
+collector.
+"""
+
+import logging
+import SocketServer
+
+import metrics
+import parser
+
+class Collector(object):
+    """
+    Collectors should inherit from this class, which provides the
+    necessary interface and helpers for implementing a collector.
+    """
+
+    def __init__(self, aggregator):
+        """
+        Initializes a collector. Subclasses can override this with custom
+        parameters if they want, but they _must_ call the superclass
+        init method.
+        """
+        self.aggregator = aggregator
+
+    def run(self):
+        """
+        This method must be implemented by collectors, and is called
+        when the collector should be started. This method should block
+        forever while the collector runs.
+        """
+        raise NotImplementedError("run must be implemented")
+
+    def shutdown(self):
+        """
+        This method will be called by a second thread to notify the
+        collector to shutdown, which it should immediately and gracefully.
+        """
+        raise NotImplementedError("shutdown must be implemented")
+
+    def parse_metrics(message, ignore_errors=False):
+        """
+        Given a raw message of metrics split by newline characters, this will
+        parse the metrics and return an array of metric objects.
+
+        This will raise a :exc:`ValueError` if any metrics are invalid, unless
+        ``ignore_errors`` is set to True.
+        """
+        results = []
+        for (key, value, metric_type, flag) in parser.parse_message(message):
+            if metric_type in metrics.METRIC_TYPES:
+                # Create and store the metric object
+                metric = metrics.METRIC_TYPES[metric_type](key, value, flag)
+                results.append(metric)
+            else:
+                # TODO: Log here?
+                if not ignore_errors:
+                    raise ValueError("Invalid metric received")
+
+        return results
+
+    def add_metrics(metrics):
+        """
+        Adds the given array of metrics to the aggregator.
+        """
+        self.aggregator.add_metrics(metrics)
+
+class UDPCollector(Collector):
+    """
+    This is a collector which listens for UDP packets, parses them,
+    and adds them to the aggregator.
+    """
+
+    def __init__(self, host, port, **kwargs):
+        super(UDPCollector, self).__init__(**kwargs)
+
+        self.server = UDPCollectorSocketServer((host, port), UDPCollectorSocketHandler)
+        self.logger = logging.getLogger("statsite.udpcollector")
+
+    def run(self):
+        # Run the main server forever, blocking this thread
+        self.server.serve_forever()
+
+    def shutdown(self):
+        # Tell the main server to stop
+        self.server.shutdown()
+
+class UDPCollectorSocketServer(SocketServer.UDPServer):
+    """
+    The SocketServer implementation for the UDP collector.
+    """
+
+    allow_reuse_address = True
+
+    def __init__(self, *args, **kwargs):
+        self.collector = kwargs["collector"]
+        del kwargs["collector"]
+
+        SocketServer.UDPServer.__init__(self, *args, **kwargs)
+
+class UDPCollectorSocketHandler(SocketServer.RequestHandler):
+    """
+    Simple handler that receives UDP packets, parses them, and adds
+    them to the aggregator.
+    """
+
+    def handle(self):
+        # Get the message
+        message, _ = self.request
+
+        # Add the parsed metrics to the aggregator
+        metrics = self.server.collector.parse_metrics(message, ignore_errors=True)
+        self.server.collector.add_metrics(metrics)
---
+++ tests/unit/test_collector.py
@@ -0,0 +1,52 @@
+"""
+Contains tests for the collector base class.
+"""
+
+import pytest
+from statsite.metrics import KeyValue, Timer
+from statsite.collector import Collector
+
+class TestCollector(object):
+    def test_stores_aggregator(self):
+        """
+        Tests that collectors will store aggregator objects.
+        """
+        agg = object()
+        assert agg is Collector(agg).aggregator
+
+    def test_parse_metrics_succeeds(self):
+        """
+        Tests that parsing metrics succeeds and returns an array
+        of proper metric objects.
+        """
+        message = "\n".join(["k:1|kv", "j:27|ms"])
+        results = Collector(None).parse_metrics(message)
+
+        assert isinstance(results[0], KeyValue)
+        assert isinstance(results[1], Timer)
+
+    def test_parse_metrics_errors(self):
+        """
+        Tests that parsing metrics will throw an exception if any
+        of the metrics are invalid.
+        """
+        message = "k:1|nope"
+
+        with pytest.raises(ValueError):
+            Collector(None).parse_metrics(message)
+
+    def test_parse_metrics_suppress_error(self):
+        """
+        Tests that parsing metrics will suppress errors if requested.
+        """
+        message = "k:1|nope"
+        results = Collector(None).parse_metrics(message, ignore_errors=True)
+
+        assert 0 == len(results)
+
+    def test_add_metrics(self):
+        """
+        Tests that add_metrics successfully adds an array of metrics to
+        the configured aggregator.
+        """
+        pass
